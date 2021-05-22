0301b55a1c4552b53075a8d661c64b6c72e139f1
ValentynaBL <44778975+ValentynaBL@users.noreply.github.com>
2021-05-22 18:30:36 +0300

    Add files via upload

---
+++ HW_1Lesson.js
@@ -0,0 +1,30 @@
+// Ex.1
+
+let cat = 'cat';
+let dog = 'dog';
+let temp = cat;
+cat = dog;
+dog = temp;
+console.log(cat);
+console.log(dog);
+
+// Ex.2
+
+const company = 'Brightlocal';
+let position = 'QA';
+console.log(company);
+console.log(position);
+
+// Ex.3
+
+const isProductCompany = true;
+let projectName;
+console.log(typeof isProductCompany);
+console.log(typeof company);
+console.log(typeof position);
+
+
+// Ex.4
+
+projectName = 'KP';
+
